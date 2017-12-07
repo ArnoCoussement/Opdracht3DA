9d155605d7410e12d1c35acaf9b9e8316ed86429
arnocoussement <arnocoussement@gmail.com>
2017-12-07 14:50:21 +0100

    added docker files etc.

---
+++ src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting License Server"
+echo "********************************************************"
+java -jar /usr/local/RatingService/@project.build.finalName@.jar
